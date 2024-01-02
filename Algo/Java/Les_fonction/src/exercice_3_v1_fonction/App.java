c79b7ab9ce3f59b417833d2d09e82ef2ad61b8d4
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-01-02 15:59:55 +0100

    exercice 3 fini et exerceci 4 en cours

---
+++ Algo/Java/Les_fonction/src/exercice_3_v1_fonction/App.java
@@ -0,0 +1,35 @@
+package exercice_3_v1_fonction;
+
+import java.util.Scanner;
+
+public class App {
+	
+	
+	public static String mot (String supp ) {
+		String mot ;
+		int i;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Saisir un mot ou une phare : ");
+        mot = sc.nextLine();
+
+        System.out.println("Saisir un caractère à supprimer : ");
+        supp = sc.nextLine();
+
+        for(i = 0; i<= mot.length()-1;i++) {
+            if (mot.charAt(i) == supp.charAt(0)) {
+                 mot = mot.substring(0, i) + mot.substring(i + 1);
+             }
+        }
+		sc.close();
+		return mot;
+	}
+	
+    public static void main(String[] args) {
+    	String mot;
+    	String supp = null;
+    	
+    	mot = mot(supp);
+        System.out.println(mot);
+    }
+}
