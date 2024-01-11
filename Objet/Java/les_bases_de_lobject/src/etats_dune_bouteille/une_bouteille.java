0f523b107d805f25e13f910f322e90da278fc390
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-01-11 16:03:36 +0100

    exerceice sur la bouteille en cours

--- Objet/Java/les_bases_de_lobject/src/etats_dune_bouteille/une_bouteille.java
+++ Objet/Java/les_bases_de_lobject/src/etats_dune_bouteille/une_bouteille.java
@@ -1,5 +1,56 @@
 package etats_dune_bouteille;
 
 public class une_bouteille {
+	
+	private String nom ;
+	private final double capaciteEnL ;
+	private double liquide ;
+	Boolean estOuverte ; 
+	
+	public boolean ouvrir() {
+		
+		if(!estOuverte) {
+			estOuverte = true ;
+			return true ;
+		}
+		else {
+			return false ;
+		}
+	}
+	
+	public boolean fermer () {
+		
+		if (estOuverte) {
+			estOuverte =false;
+			return true ;
+		}
+		else {
+			return false ;
+		}
+	}
+	
+	public boolean remplirTout() {
+		
+		if (liquide  < capaciteEnL ) {
+			liquide = capaciteEnL ;
+			return true ;
+		}
+		else {
+			return false ;
+		}
+	}
+	public boolean remplir (double quantiteEnL) {
+		
+		if (liquide  < capaciteEnL) {
+			liquide = liquide+quantiteEnL ;
+			return true ; 
+		}
+		else {
+			return false ;
+		}
+			
+		
+	}
+	
 
 }
