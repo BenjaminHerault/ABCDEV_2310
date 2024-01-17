448f53ab9f4e634b58587282c040f60b363704ee
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-01-17 11:35:49 +0100

    UML et code Java fini porte de garage

--- Objet/Java/MODELISATION_DIAGRAMME_DE_CLASSES_ABC/src/porteDeGarage/App.java
+++ Objet/Java/MODELISATION_DIAGRAMME_DE_CLASSES_ABC/src/porteDeGarage/App.java
@@ -3,8 +3,20 @@ package porteDeGarage;
 public class App {
 
 	public static void main(String[] args) {
-
-
+			
+		PorteDeGarage PorteDeGarageDefaut = new PorteDeGarage();
+		PorteDeGarage PorteDeGarageRef = new PorteDeGarage(false , false , true , 0);
+		
+		//boolean test1 = PorteDeGarageRef.ouvrir();
+		
+		//boolean test2 = PorteDeGarageRef.fermer();
+		
+		//boolean test3 = PorteDeGarageRef.deverrouiller();
+		
+		//boolean test4 = PorteDeGarageRef.verrouiller();
+		
+		boolean test5 = PorteDeGarageRef.ouvrirPartiellement(50);
+		
 	}
 
 }
--- Objet/Java/MODELISATION_DIAGRAMME_DE_CLASSES_ABC/src/porteDeGarage/PorteDeGarage.java
+++ Objet/Java/MODELISATION_DIAGRAMME_DE_CLASSES_ABC/src/porteDeGarage/PorteDeGarage.java
@@ -2,4 +2,87 @@ package porteDeGarage;
 
 public class PorteDeGarage {
 
+	// declration des attributs
+	
+	private boolean estFermer ;
+	private boolean estVerrouiller ;
+	private boolean estPartiellementOuverte ;
+	private double estEnDegre ; 
+	
+		
+	public PorteDeGarage () {
+		
+		estFermer = false ;
+		estVerrouiller = false ;
+		estPartiellementOuverte = false ;
+		estEnDegre = 0 ;
+	}
+	public PorteDeGarage( boolean _estFermer , boolean _estVerrouiller, 
+						  boolean _estPartiellementOuverte , double _estEnDegre ) {
+		
+		this.estFermer = _estFermer ;
+		this.estVerrouiller = _estVerrouiller ;
+		this.estPartiellementOuverte = _estPartiellementOuverte ;
+	}
+	public boolean ouvrir() {
+		if(estFermer & !estVerrouiller) {
+			this.estFermer = false ;
+			this.estEnDegre = 90 ;
+			return true ;
+		}
+		else {
+			return false ;
+		}
+	}
+	public boolean fermer () {
+		if (!estFermer & !estVerrouiller ) {
+			this.estFermer = true ;
+			this.estEnDegre = 0 ;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public boolean deverrouiller() {
+		if(estVerrouiller & estFermer) {
+			this.estVerrouiller = false ;
+			return true ;
+		}
+		else {
+			return false ;
+		}
+	}
+	public boolean verrouiller() {
+		if(!estVerrouiller & estFermer ) {
+			this.estVerrouiller = true ;
+			return false;
+		}
+		else {
+			return true;
+		}	
+	}
+	public boolean ouvrirPartiellement( double degreOuverture) {
+		
+		double degre = degreOuverture;
+		boolean VeriferOuvrir = false;
+		double min = 0 ;
+		double max = 90;
+		
+			if (!estFermer & !estVerrouiller ){
+				if(degre >= min || degre <= max) {
+					this.estPartiellementOuverte = true;
+					this.estEnDegre = degreOuverture;
+					VeriferOuvrir = true ;
+				}
+				else if  ( degre == min || degre == max) {
+					this.estPartiellementOuverte = false ;
+					}
+				else {
+					VeriferOuvrir = false;
+				}
+			}
+			return VeriferOuvrir; 
+	}
+	
 }
