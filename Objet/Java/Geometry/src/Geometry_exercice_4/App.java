d7f73125e6887b041f3774b2604961af65e2ea40
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-01-31 15:54:32 +0100

    exercice robot en cours

--- Objet/Java/Geometry/src/Geometry_exercice_4/App.java
+++ Objet/Java/Geometry/src/Geometry_exercice_4/App.java
@@ -4,10 +4,11 @@ public class App {
 
 	public static void main(String[] args) {
 		Point point = new Point(0, 0) ;
-		Cercle cercle = new Cercle (point, 5);
+		Cercle cercle = new Cercle (point, 2.5);
+		Point pointAaa = new Point(0,2.5);
 		
 		cercle.afficher();
-		
+		cercle.testAppartenance(pointAaa);
 		
 	}
 
--- Objet/Java/Geometry/src/Geometry_exercice_4/Cercle.java
+++ Objet/Java/Geometry/src/Geometry_exercice_4/Cercle.java
@@ -9,24 +9,31 @@ public class Cercle {
 		rayon = _rayon ;
 	}
 	public double perimetre () {
-		double perimetre = 2 *Math.PI* rayon ;
+		double perimetre = 2 *Math.PI* this.rayon ;
 		perimetre = Math.round(perimetre*1000.0)/1000.0;
 		return perimetre ;
 	}
 	public double surface () {
-		double surface = (rayon*rayon)*Math.PI ;
+		double surface = (this.rayon*this.rayon)*Math.PI ;
 		surface = Math.round(surface*1000.0)/1000.0;
 		return surface;
 	}
-	
-	public void testAppartenance(double distance) {
-		distance = Math.sqrt(ordonne - )
+	public void testAppartenance(Point _point) {
+		double distance = Math.sqrt(Math.pow((_point.getAbscisse()
+						 -this.centre.getAbscisse()),2) + 
+						  Math.pow((_point.getOrdonnee()
+						 -this.centre.getOrdonnee()),2));
+		if (distance == this.rayon) {
+			System.out.println("Le point A appartien au cercle !");
+		}
+		else {
+			System.out.println("Le point A n'appartien pas au cercle ! ! !");
+		}
 	}
-	
 	public void afficher() {
 		System.out.println("Le perimetre du cercle est : " 
 							+ this.perimetre()+"." + 
-							"La surface du cercle est : " + 
-							this.surface()+".");
+							"La surface du cercle est : "  
+							+this.surface()+".");
 	}
 }
